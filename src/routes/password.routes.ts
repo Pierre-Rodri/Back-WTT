f25ed42dc81054d3773ed064561f0d3bf8a6b5d7
Pierre-Rodri <solizcespedesrodrigo@gmail.com>
2025-04-28 14:30:35 -0400

    correcion de rutas

--- src/routes/password.routes.ts
+++ src/routes/password.routes.ts
@@ -1,13 +1,14 @@
-import { Router } from 'express';
+import { Router, RequestHandler } from 'express';
 import { recoverPassword } from '../controllers/password.controller';
 import { verifyCode } from '../controllers/verifyCodeController';
 import { resetPassword } from '../controllers/resetPasswordController';
 
 const router = Router();
 
-router.post('/recover-password', recoverPassword);
-router.post('/verify-code', verifyCode);
-router.post('/reset-password', resetPassword);
+router.post('/recover-password', recoverPassword as RequestHandler);
+router.post('/verify-code', verifyCode as RequestHandler);
+router.post('/reset-password', resetPassword as RequestHandler);
 
 export default router;
+
 
